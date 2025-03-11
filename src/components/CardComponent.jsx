80c0562569fdef640ec5128b0948892c06a50edc
Breno Façanha <brenofacanha.ti@gmail.com>
2025-03-11 20:00:41 -0300

    pegando dados do cartão da api

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,7 +1,7 @@
 import { LuNfc } from "react-icons/lu";
 import { FcSimCardChip } from "react-icons/fc";
 
-export default function CardComponent(){
+export default function CardComponent({nome, numero}){
     return (
         <div className="w-[600px] h-[340px] bg-slate-950 rounded-[10px] mt-[40px] ml-[70px]">
                   <div className="flex justify-between pl-5">
@@ -18,12 +18,12 @@ export default function CardComponent(){
                   <div className="text-white text-2xl w-full flex justify-left pl-10 pt-6">
                     <p className="text-[40px]">
                       {/* {numeroCartao == false ? "0000 0000 0000 0000" : numeroCartao} */}
-                      {"0000 0000 0000 0000"}
+                      {numero || "0000 0000 0000 0000"}
                     </p>
                   </div>
                   <div className="text-white pl-10 text-[40px]">
                     {/* {nomeCartao == false ? "Nome no cartão" : nomeCartao} */}
-                    {"Nome no cartão"}
+                    {nome || "Nome no cartão"}
                   </div>
                 </div>
     )
--- src/pages/cartoes/index.jsx
+++ src/pages/cartoes/index.jsx
@@ -1,16 +1,32 @@
 import { LuNfc } from "react-icons/lu";
 import { FcSimCardChip } from "react-icons/fc";
 import CardComponent from "../../components/cardComponent";
+import { useEffect, useState } from "react";
+import instance from "../../api/instance";
 
 export default function PageCartoes() {
+  const [cartoes, setCartoes] = useState([])
+
+  async function getCartoes() {
+    const response = await instance.get("/creditcards")
+    setCartoes(response.data)
+  }
+
+  useEffect( () => {
+    getCartoes()
+  },[])
+
+
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="w-full h-[15%] flex items-center justify-center">
         <h1 className="text-[28px] font-bold">Cartões Cadastrados</h1>
       </div>
       <div className="w-full h-auto grid">
-        <CardComponent />
-        <CardComponent />
+        {cartoes.map( (event) => {
+          return <CardComponent key={event._id} nome={event.name} numero={event.number} />
+        } )}
+      
       </div>
     </div>
   );
